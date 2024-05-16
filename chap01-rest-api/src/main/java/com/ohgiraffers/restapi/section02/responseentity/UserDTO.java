55cc982aa473c8aa2e70e5eba56087f665cb2958
JH5256 <skdltmvhs2@naver.com>
2024-05-16 16:52:44 +0900

    commit

---
+++ chap01-rest-api/src/main/java/com/ohgiraffers/restapi/section02/responseentity/UserDTO.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.restapi.section02.responseentity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class UserDTO {
+
+    private int no;
+    private String id;
+    private String pwd;
+    private String name;
+    private Date entrollDate;
+
+}
+
+
